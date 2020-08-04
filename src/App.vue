2f8df782450e4c502b0c41c6549d86f79fb4a39c
Igor Tuag~ <igortuag@gmail.com>
2020-08-04 10:35:44 -0300

    Improve style

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
           @drag-start="handleDragStart('backlog', $event)"
           @drop="handleDrop('backlog', $event)"
           :get-child-payload="getChildPayload"
+          :drop-placeholder="{ className: 'placeholder' }"
         >
           <Draggable v-for="card in cards.backlog" :key="card.id">
             <Card>
@@ -24,6 +25,7 @@
           @drag-start="handleDragStart('dev', $event)"
           @drop="handleDrop('dev', $event)"
           :get-child-payload="getChildPayload"
+          :drop-placeholder="{ className: 'placeholder' }"
         >
           <Draggable v-for="card in cards.dev" :key="card.id">
             <Card>
@@ -39,6 +41,7 @@
           @drag-start="handleDragStart('test', $event)"
           @drop="handleDrop('test', $event)"
           :get-child-payload="getChildPayload"
+          :drop-placeholder="{ className: 'placeholder' }"
         >
           <Draggable v-for="card in cards.test" :key="card.id">
             <Card>
@@ -54,6 +57,7 @@
           @drag-start="handleDragStart('closed', $event)"
           @drop="handleDrop('closed', $event)"
           :get-child-payload="getChildPayload"
+          :drop-placeholder="{ className: 'placeholder' }"
         >
           <Draggable v-for="card in cards.closed" :key="card.id">
             <Card>
@@ -136,12 +140,12 @@ export default {
   display: flex;
   justify-content: flex-start;
   margin: 1.2rem 0.8rem;
+  align-items: flex-start;
 }
 
 .lane {
   background: var(--color-grey);
   width: 23rem;
-  height: 30rem;
   border-radius: 0.8rem;
   box-shadow: 0 0.1rem 0.2rem 0 rgba(33, 33, 33, 0.1);
   margin: 0 0.8rem;
@@ -151,5 +155,12 @@ export default {
 .lane-title {
   padding: 0.8rem 0.5rem;
   margin-bottom: 0.6rem;
+}
+
+.placeholder {
+  background: rgba(33, 33, 33, 0.08);
+  border-radius: 0.4rem;
+  transform: scaleY(0.85);
+  transform-origin: 0% 0%;
 }
 </style>
