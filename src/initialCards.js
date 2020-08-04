c7d7bbff6678255475e2b2f71e8334b8f16204aa
Igor Tuag~ <igortuag@gmail.com>
2020-08-04 08:09:06 -0300

    Initial card an layout settings

---
+++ src/initialCards.js
@@ -0,0 +1,15 @@
+export default {
+  backlog: [
+    { id: 1, text: "primeiro card" },
+    {
+      id: 2,
+      text: "segundo card",
+    },
+    { id: 3, text: "teceiro card" },
+  ],
+  dev: [
+    { id: 4, text: "quarto card" },
+    { id: 5, text: "quinto card" },
+  ],
+  testes: [{ id: 6, text: "sexto card" }],
+};
